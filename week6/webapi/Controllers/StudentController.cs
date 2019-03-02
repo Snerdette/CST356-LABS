081d075495e2b2b1ba17501b8187ef85d4765a9b
Katy LaFrance <snerdette@gmail.com>
2019-03-02 13:14:27 -0800

    week 6 folder

---
+++ week6/webapi/Controllers/StudentController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Database;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        private readonly SchoolContext _dbContext;
+
+        public StudentController(SchoolContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET api/values
+        /* 
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return getStudents();
+        }
+        */
+
+        
+         [HttpGet]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(_dbContext.Student.ToList());
+        }
+
+        /*
+        private List<Student> getStudents(){
+            return _dbContext.students;
+        }
+         */
+
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
